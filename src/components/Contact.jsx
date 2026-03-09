c8ce9b6ed60a9b976cbc370f74a950b3577d57a6
VasiKumar <kumarvasi22@gmail.com>
2026-03-09 14:33:18 +0530

    Changed contact me

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,6 +5,19 @@ function Contact({ email }) {
       <p className="contact-text">
         Have a question or want to work together? Feel free to reach out!
       </p>
+
+     
+        <p>
+          <ol>⭐ Emial - basistha@gmail.com</ol>
+
+
+
+          <ol>⭐ Ph - 7984327234</ol>
+
+          <ol>⭐ Address - Deuli , Mayurbhanj </ol>
+          
+        </p>
+      
       <a href={`mailto:${email}`} className="contact-email-link">
         &#9993; {email}
       </a>
